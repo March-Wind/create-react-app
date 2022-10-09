26e2deb69b5b15dde480011e87358f699ac87975
liuzhiyang <zhiyang1.liu@ximalaya.com>
2022-10-09 17:47:13 +0800

    fix: 复制模板

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
 #!/usr/bin/env ts-node-script
-console.log(111111);
-import { mkdirSync, writeFileSync } from "fs";
-import { join } from "path";
+import {createRequire} from 'module'
+import { mkdirSync, rmSync, writeFileSync } from "fs";
+import { join, resolve } from "path";
 import {exec} from "child_process";
 import { Listr } from "listr2";
-
 import { fsCopy } from "./tool/index.js";
 
-// const args = argv.option({
+const require = createRequire(import.meta.url);
 
-// })
 const root_dir = process.cwd();
 const template_dir = join(root_dir,'src/project-template');
-const packageJson_path = template_dir + 'package.json';
+const packageJson_path = resolve(template_dir,'package.json');
 const projectName = process.argv.slice(2,3)[0];
 const project_dir = join(root_dir,projectName);
 const task = new Listr<{}>([
   {
-    title: 'copy project template', // 复制模板项目
+    title: 'clear dir',
     task(ctx, task) {
+      rmSync(project_dir,{recursive:true})
+    },
+  },
+  {
+    title: 'copy project template', // 复制模板项目
+    async task(ctx, task) {
       // 1. 创建项目名的文件夹  
       mkdirSync(project_dir);
       // 2. 把项目模板copy到文件夹
       // 2.1 package.json 修改项目名
-      const packageJson = require(packageJson_path);
+      const packageJson = require(packageJson_path)
       packageJson.name = projectName;
       writeFileSync(packageJson_path, JSON.stringify(packageJson, null, 2), {encoding:'utf-8'})
       // 2.2 将project-template下的文件复制到项目文件夹下
-      fsCopy(projectName, project_dir);
+      fsCopy(template_dir, project_dir);
     },
   },
   {
@@ -39,4 +43,4 @@ const task = new Listr<{}>([
 ])
 
 
-task.run();
+await task.run();
--- src/tool/index.ts
+++ src/tool/index.ts
@@ -1,3 +1,4 @@
+import path from "path";
 import { copyFile, copyFileSync, fstat,mkdirSync,readdirSync, readFileSync, statSync } from "fs"
 import util from 'node:util'
 
@@ -7,7 +8,7 @@ import util from 'node:util'
  * @param {string} path
  * @return {*}  {boolean}
  */
-const hasFile = (path:string):boolean => {
+const isFile = (path:string):boolean => {
   const state = statSync(path);
   return state.isFile()
   
@@ -18,7 +19,7 @@ const hasFile = (path:string):boolean => {
  * @param {string} path
  * @return {*}  {boolean}
  */
-const hasDirectory =  (path:string):boolean => {
+const isDirectory =  (path:string):boolean => {
   const state = statSync(path)
   return state.isDirectory()
 }
@@ -31,7 +32,7 @@ const hasDirectory =  (path:string):boolean => {
  * @return {*} 
  */
 const _readFileSnyc = (path:string, options?:Parameters<typeof readFileSync>[1]) => {
-  const existence = hasFile(path);
+  const existence = isFile(path);
   if(existence){
     return readFileSync(path,options);
   }
@@ -53,6 +54,72 @@ const readJsFileSnyc = (path:string) => {
   return content;
 }
 
+interface DFSTree {
+  value?: any;
+  children?: DFSTree[];
+}
+interface DFSOptons {
+  getChildrenFn?:(tree:any) => any[];
+  traverse?:(node:any) => boolean;
+  handleNode?:(node:any) => void;
+}
+// 深度优先遍历
+function DFS (tree:any, options:DFSOptons)
+function DFS (tree:DFSTree)
+function DFS (tree:DFSTree |any,options?:DFSOptons){
+  const defaultOptions:DFSOptons = {
+    getChildrenFn: (node) => {
+      return node.children
+    },
+    traverse:(node) => {
+      return !!node.children
+       
+    },
+    handleNode:(node) => {
+      return;
+    }
+  }
+  const _options = {
+    ...defaultOptions,
+    ...options
+  }
+  const {getChildrenFn, traverse, handleNode} = _options;
+  const children = getChildrenFn(tree);
+  const len = children.length;
+  for(let i = 0; i <len; i++){
+    const node = children[i];
+    // 针对节点进行业务处理
+    handleNode(node);
+    // 逻辑控制：是否继续遍历自己的孩子
+    if(traverse(node)){
+      DFS(node,options);
+    }
+  }
+}
+
+const _fsCopy = (source:string,destination:string) => {
+  const options:DFSOptons = {
+    getChildrenFn(node) {
+        return readdirSync(node);
+    },
+    traverse(node){
+      return isDirectory(node)
+    },
+    handleNode(node) {
+      if(isFile(node)){
+        copyFileSync(node,destination) 
+      }
+      if(isDirectory(node)){
+        const dir = (node as string).replace(source,'');
+        const _dest = path.resolve(destination,dir)
+        mkdirSync(_dest,{recursive:true});
+      }
+    },
+
+  }
+  DFS(source,options)
+}
+
 
 // 递归copy文件到某个目录下
 const fsCopy = (source:string,destination:string) => {
@@ -62,10 +129,11 @@ const fsCopy = (source:string,destination:string) => {
     for(let i =0; i < files.length; i++){
       const _src = src + '/' + files[i];
       const _dest = dest + '/' + files[i];
-      if(hasFile(_src)){
+      if(isFile(_src)){
+        debugger
        copyFileSync(_src,_dest) 
       }
-      if(hasDirectory(_src)){
+      if(isDirectory(_src)){
         mkdirSync(_dest,{recursive:true});
         recursive(_src,_dest);
       }
@@ -76,8 +144,8 @@ const fsCopy = (source:string,destination:string) => {
 }
 
 export {
-  hasFile,
-  hasDirectory,
+  isFile,
+  isDirectory,
   _readFileSnyc,
   readJsFileSnyc,
   fsCopy
